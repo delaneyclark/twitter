8acc2c8d54bf0d39bd12403e3b352a26687bfe01
delaneyclark <delaneyclark6@gmail.com>
2015-04-07 21:16:34 -0400

    JSON to js
    basic json to js, working

---
+++ code.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		      $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/thedress", function(response) {
+						
+						console.log(response);
+						
+		      });
+			
+			})
