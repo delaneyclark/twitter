9daa35fff3d939acc150d0627b1568eab75ae67c
delaneyclark <delaneyclark6@gmail.com>
2015-04-07 22:01:52 -0400

    Possible Functions
    Added non working but possible ideas for taking numbers and inserting
    as #HEXCOD

--- code.js
+++ code.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-		      $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/thedress", function(response) {
+		      $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/color", 
+		      	function(response) {
 						
 						console.log(response);
+
+						var idNum = response.statuses.id_str;
+						var statNum = response.statuses.statuses_count;
+
+						console.log(idNum);
+						console.log(colorNum);
 						
 		      });
 			
