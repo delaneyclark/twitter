4c513c6cda075d1c68fa9036602f4b0b489716b6
delaneyclark <delaneyclark6@gmail.com>
2015-04-13 18:44:20 -0400

    div boxes forming
    but no colors

--- code.js
+++ code.js
@@ -2,26 +2,33 @@ $(document).ready(function(){
               $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/color", 
                   function(response) {
                         
+           
                         for(var i=0; i<15; i++) {
   							// console.log(response.statuses[i].user.statuses_count);
+
+
 						
 
-						if (response.statuses[i].user.statuses_count.length = 1) {
-							$("document.body.div.style.backgroundColor") = "#" + response.statuses[i].user.statuses_count + "ababa"; 
-						} else if (response.statuses[i].user.statuses_count.length = 2) {
+							
+
+						if (response.statuses[i].user.statuses_count.length === 1) {
+							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "ababa></div>" )
+						} else if (response.statuses[i].user.statuses_count.length === 2) {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "abab></div>" )
-						} else if (response.statuses[i].user.statuses_count.length = 3) {
+						} else if (response.statuses[i].user.statuses_count.length === 3) {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "aba></div>" )
-						} else if (response.statuses[i].user.statuses_count.length = 4) {
+						} else if (response.statuses[i].user.statuses_count.length === 4) {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "ab></div>" )
-						} else if (response.statuses[i].user.statuses_count.length = 5) {
+						} else if (response.statuses[i].user.statuses_count.length === 5) {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "a></div>" )
-						} else if (response.statuses[i].user.statuses_count.length = 6) {
+						} else if (response.statuses[i].user.statuses_count.length === 6) {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "></div>" )
 						} else {
 							$("body").append("<div style=color:#" + response.statuses[i].user.statuses_count + "></div>" )
 						}
-                        };
+					};
+
+                        
               });
             
             })
