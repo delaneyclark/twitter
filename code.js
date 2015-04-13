b463ced0e6c14b85f9b1d3df9fbdd529d95277d5
delaneyclark <delaneyclark6@gmail.com>
2015-04-13 16:09:47 -0400

    Non-working
    for -> if statement, var doesn’t work. . .

--- code.js
+++ code.js
@@ -2,13 +2,25 @@ $(document).ready(function(){
               $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/color", 
                   function(response) {
                         
-                        console.log(response);
-
-                        var colorNum = response;
-
                         for(var i=0; i<15; i++) {
-  							console.log(response.statuses[i].user.statuses_count);
+  							var colNum = console.log(response.statuses[i].user.statuses_count);
 						};
+
+						if (colNum.length = 1) {
+							$("body").append("<div style=color:#" + colNum + "ababa></div>" )
+						} else if (colNum.length = 2) {
+							$("body").append("<div style=color:#" + colNum + "abab></div>" )
+						} else if (colNum.length = 3) {
+							$("body").append("<div style=color:#" + colNum + "aba></div>" )
+						} else if (colNum.length = 4) {
+							$("body").append("<div style=color:#" + colNum + "ab></div>" )
+						} else if (colNum.length = 5) {
+							$("body").append("<div style=color:#" + colNum + "a></div>" )
+						} else if (colNum.length = 6) {
+							$("body").append("<div style=color:#" + colNum + "></div>" )
+						} else {
+							$("body").append("<div style=color:#" + colNum + "></div>" )
+						}
                         
               });
             
