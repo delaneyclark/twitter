3ec599d3ec769162c755f606bf7f08b9c087f306
delaneyclark <delaneyclark6@gmail.com>
2015-04-13 15:47:49 -0400

    console.log status/color string
    working, print status string

--- code.js
+++ code.js
@@ -1,31 +1,15 @@
 $(document).ready(function(){
-		      $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/color", 
-		      	function(response) {
-						
-						console.log(response);
+              $.getJSON("http://cooper-union-search-proxy.herokuapp.com/twitter/search/color", 
+                  function(response) {
+                        
+                        console.log(response);
 
-						var idNum = response.statuses.id_str {
-							function(idstr) {
-								//if (take last 6 numbers of string) {
-									var out= "#" + idstr;
-								}
-							}
-						}
-						var statNum = response.statuses.statuses_count {
-							function(status) {
-								//if (take first 6 of string) {
-								//	var outs= "#" + stuatus
-								// } else if (i < 6) {
-								// 	var outs= "#" + status + "AB"	
-								//} else {
-								//	var outs= "#" + status	
-								//}
-							}
-						} ;
+                        var colorNum = response;
 
-						console.log(idNum);
-						console.log(colorNum);
-						
-		      });
-			
-			})
+                        for(var i=0; i<15; i++) {
+  							console.log(response.statuses[i].user.statuses_count);
+						};
+                        
+              });
+            
+            })
