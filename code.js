643542477b56c083796052f6a005af97aa688d88
delaneyclark <delaneyclark6@gmail.com>
2015-04-18 13:57:12 -0400

    WORKING WITH COLORS
    and bigger box size

--- code.js
+++ code.js
@@ -11,20 +11,20 @@ $(document).ready(function(){
 
 							
 
-						if (response.statuses[i].user.statuses_count.length === 1) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "aaaaa\"></div>" )
-						} else if (response.statuses[i].user.statuses_count.length === 2) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "aaab\"></div>" )
-						} else if (response.statuses[i].user.statuses_count.length === 3) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "aaa\"></div>" )
-						} else if (response.statuses[i].user.statuses_count.length === 4) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "aa\"></div>" )
-						} else if (response.statuses[i].user.statuses_count.length === 5) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "a\"></div>" )
-						} else if (response.statuses[i].user.statuses_count.length === 6) {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "\"></div>" )
+						if (response.statuses[i].user.statuses_count.toString().length == 1) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "aaaaa\"></div>" )
+						} else if (response.statuses[i].user.statuses_count.toString().length == 2) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "aaab\"></div>" )
+						} else if (response.statuses[i].user.statuses_count.toString().length == 3) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "aaa\"></div>" )
+						} else if (response.statuses[i].user.statuses_count.toString().length == 4) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "aa\"></div>" )
+						} else if (response.statuses[i].user.statuses_count.toString().length == 5) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "a\"></div>" )
+						} else if (response.statuses[i].user.statuses_count.toString().length == 6) {
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "\"></div>" )
 						} else {
-							$("body").append("<div style=\"color:#" + response.statuses[i].user.statuses_count + "\"></div>" )
+							$("ul").append("<div style=\"background-color:#" + response.statuses[i].user.statuses_count + "\"></div>" )
 						}
 					};
 
